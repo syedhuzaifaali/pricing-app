54474a1b316ccc7b85ae6b94ec3d166412948ba1
syedhuzaifaali <31947570+syedhuzaifaali@users.noreply.github.com>
2023-02-08 15:25:32 +0500

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/Headers'
+import { Pricing } from '../components/Pricing';
+
+export default function Home() {
+  return (
+    <>
+       <Header />
+      <Pricing />
+    </>
+  )
+}
